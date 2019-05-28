d1afa1212b969ae76ecea2f256626a2f3e2bf87a
Jim Fowler <fowler@math.osu.edu>
2019-05-27 22:54:37 -0400

    republishing

--- make-thumbnails.sh
+++ make-thumbnails.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 thumbnailify() {
-    mutool draw -r 180 -o - $1 0 | convert -gravity Center -crop 1000x180+0+0 png:- "${1%.*}.png"
+    mutool draw -r 40 -o - $1 0 | convert -gravity Center -crop 500x500+0+0 png:- "${1%.*}.png"
 }
 
 find cv -iname '*.pdf' | while read file; do thumbnailify "$file"; done
